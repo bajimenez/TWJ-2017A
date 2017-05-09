5523bc26cc7b29f460de7af262599bb7e2bfc1e8
bajimenez <brayan.jimenez@epn.edu.ec>
2017-05-09 16:30:11 -0500

    TemplateStrings

--- 02-javascript/app.js
+++ 02-javascript/app.js
@@ -1,2 +1,19 @@
 // log es un funcion que adminte strigs
 console.log('hello world');
+var  brayan={
+    nombre: "brayan",
+    apellido: "jimenez",
+    fechaNacimiento: new Date (),
+    imprimir:function () {
+        console.log(this.nombre,this.nombre);
+    },
+    ano:function () {
+        return  this.fechaNacimiento.getFullYear();
+    },
+    diferenciaEdad:function (edadAComprar) {
+        return Math.abs(edadAComprar-this.fechaNacimiento.getFullYear())
+    }
+}
+
+brayan.imprimir();
+console.log(brayan.diferenciaEdad(2000));
